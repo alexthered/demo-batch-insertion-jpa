11aa8b30c29cd2be551cfabfab8347bc1778d53c
Hiep Doan <hiep.dv179@gmail.com>
2018-11-04 11:47:35 +0100

    initialize the projects

---
+++ src/main/java/com/alexthered/demobatchinsertionjpa/DemoBatchInsertionJpaApplication.java
@@ -0,0 +1,12 @@
+package com.alexthered.demobatchinsertionjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBatchInsertionJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBatchInsertionJpaApplication.class, args);
+    }
+}
